f1bbfc76f20c33c452f4318fde17ed25972784be
MuhdNurKamal <mnkamal95@gmail.com>
2020-03-23 18:18:54 +0700

    Update app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import logging
 import sys
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-from pprint import pprint as print
+from pprint import pprint
 from gensim.summarization import summarize
 
 text = ("".join([line for line in sys.stdin]))
 
-print(summarize(text, ratio=0.01) )
+pprint(summarize(text, ratio=0.1) )
